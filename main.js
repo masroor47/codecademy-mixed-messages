14cc9448258ab7907ac778d463e7fae90bb78e60
Masroor Khonkhodzhaev <masroor47@icloud.com>
2022-06-07 12:51:53 -0400

    edit typos, clean up code

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 
 // fortune teller kinda thing made up of three sections in a sentance.
-// EXAMPLE Good news! You have an unexpected opportunity coming along.
+// EXAMPLE Good news! You have an unexpected opportunity coming tomorrow.
+
 // First part says whether it's good, concerning, sad, etc.
 // Whether it's good or bad affects the latter two parts. 
 // Second part gives a brief description. 'something intriguing', 'an unexpected gift', etc.
 // Third part is time. 'in two weeks', 'in the next day', 'over the next season'
-
-
-let finalMessage = [];
 
 const goodBeginning = [
     'Good news!', 
@@ -44,7 +42,7 @@ const badMiddle = [
 
 const time = [
     'today', 
-    'tommorow', 
+    'tomorrow', 
     'during this week', 
     'over the next month', 
     'during this season', 
@@ -52,24 +50,16 @@ const time = [
 ];
 
 
-// returns a random element of the inputted array 
+// returns a random element of the inputted array regardless of the size of the array
 const pickRandom = (array) => {
     const randomIndex = Math.floor(Math.random() * array.length);
-    console.log('the randomly picked item is: ', array[randomIndex])
+    //console.log('the randomly picked item is: ', array[randomIndex])
     return array[randomIndex]
 }
 
-// const decideBeginning = (goodOrBad) => {
-//     if (goodOrBad) {
-//         console.log('picking good')
-//         return pickRandom(goodBeginning);
-//     }
-//     console.log("picking bad")
-//     return pickRandom(badBeginning)
-// }
-
 const buildMessage = () => {
     const message = []
+    // randomly deciding whether message will be good or bad
     if (Math.random() < 0.5) {
         message.push(pickRandom(goodBeginning))
         message.push(pickRandom(goodMiddle))
@@ -81,20 +71,6 @@ const buildMessage = () => {
     message.push(pickRandom(time) + '.');
     return message
 }
-
-
-
-
-// randomly decides if the fortune is gonna be good or bad
-//const goodNews = Math.random() < 0.5;
-
-// adding first part to the final message array
-//finalMessage.push(decideBeginning(goodNews));
-
-
-
-//finalMessage = finalMessage.join(' ');
-//console.log(finalMessage);
 
 
 let myMessage = buildMessage()
